f30f3bcd007b4a7de6a319d68a3e7fcde86dd059
brunobrolesi <brunohsbrolesi@gmail.com>
2021-10-29 11:20:22 -0300

    test: ensure BotJoinController calls addServer with correct values

--- src/presentation/controllers/bot-join.spec.ts
+++ src/presentation/controllers/bot-join.spec.ts
@@ -1,25 +1,62 @@
+import { AddServer, AddServerModel } from '../../domain/usecases/add-server'
 import { BotJoinController } from './bot-join'
 
-const makeSut = (): BotJoinController => {
-  return new BotJoinController()
+const makeAddServer = (): AddServer => {
+  class AddServerStub implements AddServer {
+    async add (server: AddServerModel): Promise<void> {
+      return await new Promise(resolve => resolve())
+    }
+  }
+  return new AddServerStub()
+}
+
+interface SutTypes {
+  sut: BotJoinController
+  addServerStub: AddServer
+}
+
+const makeSut = (): SutTypes => {
+  const addServerStub = makeAddServer()
+  const sut = new BotJoinController(addServerStub)
+
+  return {
+    sut,
+    addServerStub
+  }
 }
 
 describe('Bot Join Controller', () => {
-  it('Should throw if invalid server id is provided', () => {
-    const sut = makeSut()
+  it('Should throw if invalid server id is provided', async () => {
+    const { sut } = makeSut()
     const fakeServerData = {
       id: '',
       name: 'any_name'
     }
-    expect(() => sut.handle(fakeServerData)).toThrow()
+    const promise = sut.handle(fakeServerData)
+    await expect(promise).rejects.toThrow()
   })
 
-  it('Should throw if invalid server name is provided', () => {
-    const sut = makeSut()
+  it('Should throw if invalid server name is provided', async () => {
+    const { sut } = makeSut()
     const fakeServerData = {
       id: 'any_id',
       name: ''
     }
-    expect(() => sut.handle(fakeServerData)).toThrow()
+    const promise = sut.handle(fakeServerData)
+    await expect(promise).rejects.toThrow()
+  })
+
+  it('Should call AddServer with correct values', async () => {
+    const { sut, addServerStub } = makeSut()
+    const addSpy = jest.spyOn(addServerStub, 'add')
+    const fakeServerData = {
+      id: 'valid_id',
+      name: 'valid_name'
+    }
+    await sut.handle(fakeServerData)
+    expect(addSpy).toHaveBeenCalledWith({
+      server_id: 'valid_id',
+      name: 'valid_name'
+    })
   })
 })
